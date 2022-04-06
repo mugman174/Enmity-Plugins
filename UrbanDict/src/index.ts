2914bf84594cb6d59876a35102e2daae135949c8
mugman <53713683+nature174@users.noreply.github.com>
2022-04-06 11:29:11 -0700

    Urban Dictionary plugin & update store

---
+++ UrbanDict/src/index.ts
@@ -0,0 +1,54 @@
+import {
+  EnmitySectionID,
+  ApplicationCommandInputType,
+  ApplicationCommandOptionType,
+  ApplicationCommandType,
+} from "enmity-api/commands";
+import { get } from "enmity-api/rest";
+import { Plugin, registerPlugin } from "enmity-api/plugins";
+
+const UrbanDict: Plugin = {
+  name: "UrbanDict",
+  commands: [],
+
+  onStart() {
+    command = {
+      name: "urban",
+      displayName: "urban",
+      description: "Searches urban dictionary for a word",
+      displayDescription: "Searches urban dictionary for a word",
+      options: [
+        {
+          name: "word",
+          displayName: "word",
+          description: "The word to search for",
+          displayDescription: "The word to search for",
+          type: ApplicationCommandOptionType.String,
+          required: true,
+        },
+        {
+          name: "send",
+          displayName: "send",
+          description: "Whether to send the result in a message",
+          displayDescription: "Whether to send the result in a message",
+          type: ApplicationCommandOptionType.Boolean,
+          required: false,
+        },
+      ],
+      execute: async (args, message) => {
+        word = args[0].value;
+        let res = await get(
+          `https://api.urbandictionary.com/v0/define?term=${word}`
+        ).body;
+        sendReply(res.list[0]?.definition);
+      },
+    };
+    this.commands.push(command);
+  },
+
+  onStop() {
+    this.commands = [];
+  },
+};
+
+registerPlugin(UrbanDict);
