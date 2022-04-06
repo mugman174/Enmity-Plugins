b48cd7ba6df7bb73bde5b0722c26fd86096ca00a
mugman <53713683+nature174@users.noreply.github.com>
2022-04-06 08:44:37 -0700

    plugin

--- HelloWorld/src/index.ts
+++ HelloWorld/src/index.ts
@@ -1,16 +1,50 @@
 import { Plugin, registerPlugin } from "enmity-api/plugins";
+import {
+  Command,
+  EnmitySectionID,
+  ApplicationCommandInputType,
+  ApplicationCommandOptionType,
+  ApplicationCommandType,
+} from "enmity-api/commands";
+import { sendReply } from "enmity-api/clyde";
+import { showToast, ToastMessage } from "enmity-api/toast";
 
-const ExamplePlugin: Plugin = {
-  name: "ExamplePlugin",
+const HelloWorld: Plugin = {
+  name: "HelloWorld",
   commands: [],
 
   onStart() {
-
+    const h: Command = {
+      id: "hello-world",
+      applicationId: EnmitySectionID,
+      name: "hello",
+      displayName: "hello",
+      description: "hello world",
+      displayDescription: "hello world",
+      type: ApplicationCommandType.Chat,
+      inputType: ApplicationCommandInputType.BuiltInText,
+      options: [
+        {
+          name: "toast",
+          displayName: "toast",
+          description: "Toast",
+          displayDescription: "Toast",
+          type: ApplicationCommandOptionType.String,
+          required: false,
+        },
+      ],
+      execute: async function (args, message): Promise<void> {
+        toasttext: ToastMessage = { content: String(args), source: 42069 };
+        showToast(toasttext);
+        sendReply(message.channel.id, "Hello, world!");
+      },
+    };
+    this.commands.push(h);
   },
 
   onStop() {
+    this.commands = [];
+  },
+};
 
-  }
-}
-
-registerPlugin(ExamplePlugin);
+registerPlugin(HelloWorld);
