a743fe91eca9b55e18d52f566e05b0b0fb8f4275
mugman <53713683+nature174@users.noreply.github.com>
2022-04-07 11:50:26 -0700

    Mock command plugin

---
+++ Mock/src/index.ts
@@ -0,0 +1,62 @@
+import { Plugin, registerPlugin } from "enmity-api/plugins";
+import {
+  Command,
+  EnmitySectionID,
+  ApplicationCommandInputType,
+  ApplicationCommandOptionType,
+  ApplicationCommandType,
+} from "enmity-api/commands";
+
+function slashOpt(name, description, type) {
+  return {
+    name: name,
+    displayName: name,
+    description: description,
+    displayDescription: description,
+    type: type || ApplicationCommandOptionType.String,
+    required: true,
+  };
+}
+function slashComad(id, name, description, options, func) {
+  return {
+    id: id,
+    applicationId: EnmitySectionID,
+    name: name,
+    displayName: name,
+    description: description,
+    displayDescripton: description,
+    type: ApplicationCommandType.Chat,
+    inputType: ApplicationCommandInputType.BuiltInText,
+    options: options,
+    execute: func,
+  };
+}
+
+const MockPlugin: Plugin = {
+  name: "MockPlugin",
+  commands: [],
+
+  onStart() {
+    const mock_slash = slashComad(
+      "mock",
+      "mock",
+      "MaKe yOuR TeXt lIkE ThIs",
+      [slashOpt("text", "The text to mock-ify")],
+      async function (args, message): Promise<void> {
+        input = args[0].value;
+        for (i = 0; i < input.length; i += 2) {
+          input =
+            input.substr(0, i) + input[i].toUpperCase() + input.substr(i + 1);
+        }
+        return { content: input };
+      }
+    );
+    this.commands.push(mock_slash);
+  },
+
+  onStop() {
+    this.commands = [];
+  },
+};
+
+registerPlugin(MockPlugin);
