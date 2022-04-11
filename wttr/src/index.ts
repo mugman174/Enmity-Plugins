05a3c8fd957d894fe64706dd391e9f62059e80ab
mugman <53713683+nature174@users.noreply.github.com>
2022-04-11 07:14:27 -0700

    weather

---
+++ wttr/src/index.ts
@@ -0,0 +1,48 @@
+import {
+  Command,
+  EnmitySectionID,
+  ApplicationCommandInputType,
+  ApplicationCommandOptionType,
+  ApplicationCommandType,
+} from "enmity-api/commands";
+//import { get } from "enmity-api/rest";
+import { Plugin, registerPlugin } from "enmity-api/plugins";
+import { sendReply } from "enmity-api/clyde";
+
+const wttr: Plugin = {
+  name: "wttr",
+  commands: [],
+
+  onStart() {
+    const command: Command = {
+      id: "wttr",
+      applicationId: EnmitySectionID,
+      name: "wttr",
+      displayName: "wttr",
+      description: "Get the weather",
+      displayDescription: "Get the weather",
+      type: ApplicationCommandType.Chat,
+      inputType: ApplicationCommandInputType.BuiltInText,
+      options: [
+        {
+          name: "location",
+          displayName: "location",
+          description: "Location",
+          displayDescription: "Location",
+          type: ApplicationCommandOptionType.String,
+          required: true,
+        },
+      ],
+      execute: async function (args, message): Promise<void> {
+        let embeds = [
+          { image: { url: `https://wttr.in/${args[0].value}.png` } },
+        ];
+        sendReply(message.channel.id, { embeds });
+      },
+    };
+  },
+
+  onStop() {},
+};
+
+registerPlugin(wttr);
