b9f694d899646603917736a6ee4a7b8fa71a82c5
mugman <53713683+nature174@users.noreply.github.com>
2022-09-17 14:53:27 -0700

    a lot of stuff but now there's a Decode Message context menu thing

--- InvisChat/src/index.tsx
+++ InvisChat/src/index.tsx
@@ -1,11 +1,17 @@
-const Opener = getByProps("openLazy");
-import { Toasts } from "enmity/metro/common";
+import * as Assets from "enmity/api/assets";
+import { Toasts, React } from "enmity/metro/common";
 import { Plugin, registerPlugin } from "enmity/managers/plugins";
 import manifest from "../manifest.json";
 import { inviscommand } from "./components";
 import { getByProps } from "enmity/metro";
 import { create } from "enmity/patcher";
 import { decoder } from "./utils";
+import { FormRow } from "enmity/components";
+import { sendReply } from "enmity/api/clyde";
+
+const Opener = getByProps("openLazy");
+const getLastSelectedChannelId = getByProps("getLastSelectedChannelId");
+const LazyActionSheet = getByProps("openLazy", "hideActionSheet");
 
 const FluxDispatcher = getByProps(
   "_currentDispatchActionType",
@@ -20,53 +26,52 @@ const Invis = {
 
   onStart() {
     this.commands = inviscommand;
-
-    Patcher.before(Opener, "openLazy", (_, [component, sheet]) => {
-      if (sheet === "MessageLongPressActionSheet") {
-        component.then((instance) => {
-          let func = instance.default;
-          instance.default = function (
-            { message, user, channel, canAddNewReactions },
-            _
-          ) {
-            let og = func({ message, user, channel, canAddNewReactions }, _);
-            if (
-              og.props?.children?.props?.children?.props?.children[1][0].key ==
-              "420"
-            ) {
-              return og;
-            }
-            let ButtonRow =
-              og.props?.children?.props?.children?.props?.children[1][0].type;
-            let decodeButton = (
-              <ButtonRow
-                key="420"
-                onPressRow={(_) => {
-                  Opener.hideActionSheet();
-                  alert(decoder(message.content, "password"));
-                }}
-                message="Decode message with InvisChat"
-              />
-            );
-            og.props.children.props.children.props.children[1].unshift(
-              decodeButton
-            );
-            return og;
-          };
-          return instance;
-        });
-      }
-    });
-
     const INV_DETECTION = new RegExp(
       /( \u200c|\u200d |[\u2060-\u2064])[^\u200b]/
     );
+
     let attempt = 0;
     let attempts = 3;
     FluxDispatcher.dispatch({
       type: "LOAD_MESSAGES",
     });
     const lateStarter = () => {
+      Patcher.before(
+        Opener,
+        "openLazy",
+        ({ hideActionSheet }, [component, sheet]) => {
+          if (sheet === "MessageLongPressActionSheet") {
+            component.then((instance) => {
+              Patcher.after(instance, "default", (_, args, res) => {
+                if (
+                  res.props.children().props.children.props.children[1][0]
+                    .key == "420"
+                ) {
+                  return;
+                }
+                const text = res.props.children().props.children.props
+                  .children[0].props.message.content; // why
+                const channelId = getLastSelectedChannelId.getChannelId();
+                if (!text.match(INV_DETECTION)) return;
+                res.props.children().props.children.props.children[1].unshift(
+                  <FormRow
+                    key="420"
+                    iconSource={Assets.getIDByName("ic_locked_24px")}
+                    label="Decode Message"
+                    onPress={() => {
+                      decoder(text, undefined).then((c) =>
+                        sendReply(channelId, { content: c })
+                      );
+                      hideActionSheet();
+                    }}
+                  />
+                );
+              });
+            });
+          }
+        }
+      );
+
       try {
         const MessageCreate = FluxDispatcher._actionHandlers._orderedActionHandlers.MESSAGE_CREATE.find(
           (h) => h.name === "MessageStore"
@@ -76,11 +81,17 @@ const Invis = {
         );
 
         Patcher.before(MessageCreate, "actionHandler", (_, args: any) => {
+          if (!args[0].message.content) {
+            return;
+          }
           if (args[0].message.content.match(INV_DETECTION)) {
             args[0].message.content += " [:lock:]";
           }
         });
         Patcher.before(LoadMessages, "actionHandler", (_, args: any) => {
+          if (!args[0].message.content) {
+            return;
+          }
           if (args[0].message.content.match(INV_DETECTION)) {
             args[0].message.content += " `[:lock:]`";
           }
